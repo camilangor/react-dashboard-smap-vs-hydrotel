8234169ca165321995da4b75f79592fffbed3c58
camilangor <camilangor@gmail.com>
2025-06-03 17:53:15 -0400

    Create package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "react-dashboard-smap-vs-hydrotel",
+  "version": "1.0.0",
+  "private": true,
+  "homepage": "https://camilangor.github.io/react-dashboard-smap-vs-hydrotel",
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "react-scripts": "5.0.1",
+    "recharts": "^2.8.0"
+  },
+  "devDependencies": {
+    "gh-pages": "^5.0.0"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "predeploy": "npm run build",
+    "deploy": "gh-pages -d build"
+  }
+}
